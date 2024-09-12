5692b0e3ee1f1735a38fb8b471757b87c83f06d7
Santosh <116057824+pythiansantosh@users.noreply.github.com>
2024-09-12 15:16:16 +0530

    Add files via upload

---
+++ LogArchive.ps1
@@ -0,0 +1,73 @@
+﻿#Script to move old logs to archive folder and remove files older than one month
+
+
+#Defs (Note: Change to correct paths when using in Production)
+
+$ErrorActionPreference='Stop'
+
+$verboselogdate = $(Get-Date).ToString("yyyyMMddHHMMss")
+
+$verboselog = "C:\Data\Celerion\Cleanup\$verboselogdate.log"
+
+$logfolder = "C:\Data\Celerion\Cleanup"
+
+$archivefolder = "C:\Data\Celerion\Cleanup\Archive"
+
+#Get Logs Older than a week
+$logfilestomove = Get-ChildItem -Path $logfolder | Where-Object lastwritetime -LT $((Get-Date).AddDays(-8)) 
+
+#Check and take action if log files are found
+If($logfilestomove) {
+
+Write-Output "Log files found older than 8 days" | Out-File $verboselog -Append
+$logfilestomove | Out-File $verboselog -Append
+
+    Foreach ($filetomove in $logfilestomove) {
+        
+        Try {
+            Move-Item -Path $filetomove.FullName -Destination $archivefolder -Force -Confirm:$false
+            Write-Output "Moved $($filetomove.FullName) to Archive Folder" | Out-File $verboselog -Append
+        }
+        Catch {
+        
+            Write-Output "Error moving $($filetomove.FullName) to Archive Folder" | Out-File $verboselog -Append
+        }
+    }
+
+}
+
+Else {
+
+Write-Output "Log files not found older than 8 days" | Out-File $verboselog -Append
+
+}
+
+#Clear Archive
+#Find files in archive older than a month and delete them
+$logfilestodelete=Get-ChildItem -Path $archivefolder | Where-Object lastwritetime -LT $((Get-Date).AddDays(-30)) #| Remove-Item -Force -Confirm:$false
+
+#Check and take action if log files are found
+If($logfilestodelete) {
+
+Write-Output "Log files found older than 30 days" | Out-File $verboselog -Append
+$logfilestodelete | Out-File $verboselog -Append
+
+    Foreach ($filetodelete in $logfilestodelete) {
+        
+        Try {
+            Remove-Item -Path $filetodelete.FullName -Force -Confirm:$false
+            Write-Output "Removed $($filetodelete.FullName)" | Out-File $verboselog -Append
+        }
+        Catch {
+        
+            Write-Output "Error removing $($filetodelete.FullName)" | Out-File $verboselog -Append
+        }
+    }
+
+}
+
+Else {
+
+Write-Output "Log files not found older than 30 days" | Out-File $verboselog -Append
+
+}
